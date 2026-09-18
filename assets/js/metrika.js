080326e24ec0828342a6e182e8599dfcec433d1a
Bell1488 <illailoza@gmail.com>
2026-09-19 00:39:37 +0200

    Use official Yandex Metrika tag URL

--- assets/js/metrika.js
+++ assets/js/metrika.js
@@ -1,6 +1,7 @@
 // Yandex Metrika loader. Put the real numeric counter ID in <body data-metrika-id="XXXXXXXX">.
 (() => {
   const id=Number(document.body.dataset.metrikaId||0); if(!id) return;
-  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(let j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r)return;}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
-  ym(id,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',accurateTrackBounce:true,trackLinks:true});
+  const tagUrl=`https://mc.yandex.ru/metrika/tag.js?id=${id}`;
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(let j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r)return;}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script',tagUrl,'ym');
+  ym(id,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
 })();
